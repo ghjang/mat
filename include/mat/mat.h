c10c6e5f69ec72fe37c4aa121be96cd38a540b8a
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-05-25 00:38:03 +0900

    configured initial project for mat.

---
+++ include/mat/mat.h
@@ -0,0 +1,11 @@
+#ifndef MAT_MAT_H
+#define MAT_MAT_H
+
+
+namespace mat
+{
+    
+} // namespace mat
+
+
+#endif // MAT_MAT_H
---
+++ test/mat_test.cpp
@@ -0,0 +1,9 @@
+#include "catch.hpp"
+
+#include "mat/mat.h"
+
+
+TEST_CASE("mat", "[mat]")
+{
+    
+}
