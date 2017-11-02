68a2747118dd6dcb1687d37ad9f04fac65b9d33e
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-11-02 11:55:02 +0900

    updated test cases.

--- include/mat/circular_generator.h
+++ include/mat/circular_generator.h
@@ -4,7 +4,6 @@
 
 #include <type_traits>
 #include <stdexcept>
-#include <iterator>
 #include <cassert>
 
 
--- test/circular_generator_test.cpp
+++ test/circular_generator_test.cpp
@@ -3,6 +3,9 @@
 #include "mat/circular_generator.h"
 using namespace mat;
 
+#include <array>
+#include <vector>
+#include <algorithm>
 #include <iostream>
 
 
@@ -29,4 +32,28 @@ TEST_CASE("circular generator operator () ()", "[mat]")
     CHECK(cg1() == 2);
     CHECK(cg1() == 1);
     CHECK(cg1() == 3);
+
+    std::array<int, 3> arr{ 1, 2, 3 };
+    circular_generator cg2(arr);
+    CHECK(cg2() == 1);
+    CHECK(cg2() == 2);
+    CHECK(cg2() == 3);
+    CHECK(cg2() == 1);
+    CHECK(arr[0] == 1);
+    CHECK(arr[1] == 2);
+    CHECK(arr[2] == 3);
+
+    circular_generator cg3{ std::array<int, 3>{ 3, 2, 1 } };
+    CHECK(cg3() == 3);
+    CHECK(cg3() == 2);
+    CHECK(cg3() == 1);
+    CHECK(cg3() == 3);
+}
+
+TEST_CASE("circular generator with std::generate", "[mat]")
+{
+    std::vector<int> v{ 7 };
+    std::generate(v.begin(), v.end(), circular_generator{ std::vector{ 1, 2, 3 } });
+    std::vector r{ 1, 2, 3, 1, 2, 3, 1 };
+    CHECK(std::equal(v.begin(), v.end(), r.begin()));
 }
